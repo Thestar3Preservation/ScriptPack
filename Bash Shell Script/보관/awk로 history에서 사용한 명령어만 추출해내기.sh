7f2a9ff0560a636b16d279bdb51c6c7a085147d0
별님 <w7dn1ng75r@gmail.com>
2014-12-05 23:21:25 +0900

    Update awk로 history에서 사용한 명령어만 추출해내기.sh

--- Bash Shell Script/보관/awk로 history에서 사용한 명령어만 추출해내기.sh
+++ Bash Shell Script/보관/awk로 history에서 사용한 명령어만 추출해내기.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #history에서 사용한 명령어만 추출해내기
 history | awk '{print substr($0,index($0,$2))}'
-#zip파일 내에 존재하는 파일 리스트 추출하기
-unzip -l a.zip | head -n -2 | tail -n -3 | awk '{print substr($0,index($0,$4))}'
+
 exit
